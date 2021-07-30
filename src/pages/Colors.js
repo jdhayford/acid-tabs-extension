037431661635596c83938b15faf478f8af3f03bc
Steven Yeh <say25@cornell.edu>
2021-07-30 12:15:23 -0500

    Maintain order

--- src/pages/Colors.js
+++ src/pages/Colors.js
@@ -2,9 +2,9 @@ export const COLORS = {
     'grey': '#BDC1C6',
     'yellow': '#FDD663',
     'blue': '#89B4F8',
-    'purple': '#D7ADFB',
-    'green': '#80C995',
     'red': '#F28B82',
-    'pink': '#FF8BCB',
+    'green': '#80C995',
+    'purple': '#D7ADFB',
     'cyan': '#78D9EC',
-}
+    'pink': '#FF8BCB',
+};
