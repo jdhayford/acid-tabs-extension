bfb808b14d6d41ec85d08517dc8025cb1e88f7be
Jack Hayford <jdhayford@gmail.com>
2022-12-31 09:50:26 -0500

    Fix emoji modal clouseout bug, bump to 6.5.9

--- src/pages/Popup/EmojiModal.jsx
+++ src/pages/Popup/EmojiModal.jsx
@@ -37,7 +37,7 @@ const EmojiModal = (props) => {
       style={modalStyle}
       onClick={() => handleClose()}
     >
-      <PickerWrapper onClick={(e) => e.stopPropogation()}>
+      <PickerWrapper onClick={(e) => e.stopPropagation()}>
         <Picker
           className="picker"
           autoFocus
